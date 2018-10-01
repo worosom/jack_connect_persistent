8d2e48ef24f3de7a5f717769fcec2e67347f2d3c
worosom <alexander.morosow@gmail.com>
2018-10-01 23:05:49 +0200

    initial commit

---
+++ connect_persistent.cpp
@@ -0,0 +1,124 @@
+/** @file persistent_client.c
+ *
+ * @brief A Jack-client that will watch for port registration changes and ensure that input and output-port stay connected if they are available.
+ */
+extern "C" {
+#include <jack/jack.h>
+#include <jack/types.h>
+}
+#include <iostream>           // std::cout
+#include <thread>             // std::thread
+#include <mutex>              // std::mutex, std::unique_lock
+#include <condition_variable> // std::condition_variable
+#include <chrono>
+
+jack_port_t *input_port;
+jack_port_t *output_port;
+jack_client_t *client;
+
+jack_nframes_t delay_index;
+
+std::mutex mtx;
+std::condition_variable cv;
+bool ready = false;
+
+typedef void (*JackPortRegistrationCallback)(jack_port_id_t port, int /* register */, void* arg);
+typedef void (*JackPortConnectCallback)(jack_port_id_t a, jack_port_id_t b, int connect, void *arg);
+
+  void
+registration_cb (jack_port_id_t port, int register, void* arg)
+{
+  std::unique_lock<std::mutex> lck (mtx);
+  ready = true;
+  cv.notify_all();
+}
+
+  void
+port_connect_cb (jack_port_id_t a, jack_port_id_t b, int connect, void *arg)
+{
+  std::unique_lock<std::mutex> lck (mtx);
+  ready = true;
+  cv.notify_all();
+}
+
+/**
+ * JACK calls this shutdown_callback if the server ever shuts down or
+ * decides to disconnect the client.
+ */
+  void
+jack_shutdown (void *arg)
+{
+  fprintf(stderr, "JACK shut down, exiting ...\n");
+  exit(1);
+}
+
+  int
+main (int argc, char *argv[])
+{
+  const char *client_name = "persistent";
+  const char *server_name = NULL;
+  jack_options_t options = JackNullOption;
+  jack_status_t status;
+
+
+  /* open a client connection to the JACK server */
+
+  client = jack_client_open(client_name, options, &status, server_name);
+  if (client == NULL) {
+    fprintf(stderr, "jack_client_open() failed, "
+        "status = 0x%2.0x\n", status);
+    if (status & JackServerFailed) {
+      fprintf(stderr, "Unable to connect to JACK server\n");
+    }
+    exit (1);
+  }
+  if (status & JackServerStarted) {
+    fprintf(stderr, "JACK server started\n");
+  }
+  if (status & JackNameNotUnique) {
+    client_name = jack_get_client_name (client);
+    fprintf(stderr, "unique name `%s' assigned\n", client_name);
+  }
+
+  // jack_set_client_registration_callback(client, registration_cb, 0);
+  jack_set_port_connect_callback(client, port_connect_cb, 0);
+  jack_set_port_registration_callback(client, registration_cb, 0);
+
+  /* tell the JACK server to call `jack_shutdown()' if
+     it ever shuts down, either entirely, or if it
+     just decides to stop calling us.
+     */
+
+  jack_on_shutdown (client, jack_shutdown, 0);
+
+  /* Tell the JACK server that we are ready to roll.  Our
+   * process() callback will start running now. */
+
+  if (jack_activate (client)) {
+    fprintf(stderr, "cannot activate client");
+    exit(1);
+  }
+  std::cout << "client activated" << std::endl;
+
+  /* keep running until stopped by the user */
+
+  std::unique_lock<std::mutex> lck(mtx);
+	while (true) {
+		if (jack_connect (client, argv[1], argv[2]) == 0) {
+      std::cout << "(re)connected " << argv[1] << " " << argv[2] << std::endl;
+    }
+		ready = false;
+		while (!ready) {
+			cv.wait(lck);
+		}
+	}
+
+  /* this is never reached but if the program
+     had some other way to exit besides being killed,
+     they would be important to call.
+     */
+
+  jack_client_close (client);
+  exit (0);
+}
+
